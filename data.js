20e39eb5df8418e7b598803598e6393337358690
Daniel Zhu <122238578+dzhu0@users.noreply.github.com>
2023-02-28 20:26:30 -0600

    Initial commit files
    Only the HTML and CSS are complete. Currently no functionality.

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        id: 0,
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        image: "🍕",
+        alt: "A picture of a pizza"
+    },
+    {
+        id: 1,
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        image: "🍔",
+        alt: "A picture of a hamburger"
+    },
+    {
+        id: 2,
+        name: "Beer",
+        ingredients: ["grain", "hops", "yeast", "water"],
+        price: 12,
+        image: "🍺",
+        alt: "A picture of a beer"
+    }
+]
